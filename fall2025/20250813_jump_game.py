a18d1e22fda9b488a8183d8f30397c0b7d762076
Leo <lmarkopoulos13@gmail.com>
2025-08-13 22:20:46 +0300

    august 13th, 2025 daily commit

---
+++ fall2025/20250813_jump_game.py
@@ -0,0 +1,9 @@
+def canJump(nums):
+    max_reach = 0
+    for i, jump in enumerate(nums):
+        if i > max_reach: 
+            return False
+        max_reach = max(max_reach, i + jump)
+        if max_reach >= len(nums) - 1: 
+            return True
+    return True
