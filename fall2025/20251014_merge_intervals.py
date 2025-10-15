7bc9783ee536cf88d2b824adbbee77c47df1bc53
Leo <lmarkopoulos13@gmail.com>
2025-10-14 20:57:41 -0400

    october 14th 2025

---
+++ fall2025/20251014_merge_intervals.py
@@ -0,0 +1,12 @@
+def merge(intervals):
+    intervals = sorted(intervals, key=lambda x: x[0])
+    length = len(intervals)
+    i = 1 
+    while i < len(intervals):
+        if intervals[i][0] <= intervals[i-1][1]:
+            intervals[i-1] = [intervals[i-1][0], max(intervals[i-1][1], intervals[i][1])]
+            intervals.pop(i)  
+        else:
+            i += 1
+
+    return intervals
