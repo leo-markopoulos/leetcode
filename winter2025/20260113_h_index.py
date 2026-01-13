e8135f3f175cafa80265f4cef7aa9f1e0cb76567
Leo Markopoulos <lmarkopoulos13@gmail.com>
2026-01-13 13:05:20 -0500

    january 13th 2026

---
+++ winter2025/20260113_h_index.py
@@ -0,0 +1,6 @@
+def hIndex(citations):
+    citations.sort()
+    for i in range(len(citations)):
+        if len(citations) - i <= citations[i]:
+            return len(citations) - i
+    return 0
