a029ef602f8da17f42e4580b8faf53a0e36028ff
Leo Markopoulos <lmarkopoulos13@gmail.com>
2026-01-12 22:02:43 -0500

    january 12th 2026

--- winter2025/20260112_seperate_squares_1.py
+++ winter2025/20260112_seperate_squares_1.py
@@ -1,31 +1,30 @@
-class Solution:
-    def separateSquares(self, squares: List[List[int]]) -> float:
-        total_area = sum((side * side for x, y, side in squares))
+def separateSquares(squares):
+    total_area = sum((side * side for x, y, side in squares))
 
-        sweep_events = (
-            [(y, 1, side) for x, y, side in squares] +
-            [(y + side, 0, side) for x, y, side in squares]
-        )
-        sweep_events.sort()
+    sweep_events = (
+        [(y, 1, side) for x, y, side in squares] +
+        [(y + side, 0, side) for x, y, side in squares]
+    )
+    sweep_events.sort()
 
-        active_width = 0
-        area_so_far = 0
-        last_y = 0
+    active_width = 0
+    area_so_far = 0
+    last_y = 0
 
-        for i in range(len(sweep_events)):
-            curr_y, is_starting, side = sweep_events[i]
+    for i in range(len(sweep_events)):
+        curr_y, is_starting, side = sweep_events[i]
 
-            height_change = curr_y - last_y
-            new_area = active_width * height_change
+        height_change = curr_y - last_y
+        new_area = active_width * height_change
 
-            if area_so_far + new_area >= total_area / 2:
-                needed_height = (total_area / 2 - area_so_far) / active_width
-                return last_y + needed_height
+        if area_so_far + new_area >= total_area / 2:
+            needed_height = (total_area / 2 - area_so_far) / active_width
+            return last_y + needed_height
 
-            if is_starting:
-                active_width += side
-            else:
-                active_width -= side
+        if is_starting:
+            active_width += side
+        else:
+            active_width -= side
 
-            area_so_far += new_area
-            last_y = curr_y
+        area_so_far += new_area
+        last_y = curr_y
