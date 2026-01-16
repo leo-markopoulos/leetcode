acf21fea355c27785580d6c7e3f639b624334ecb
Leo Markopoulos <lmarkopoulos13@gmail.com>
2026-01-16 08:46:29 -0500

    january 16th 2025 git status

---
+++ winter2025/20260116_valid_triangle_number.py
@@ -0,0 +1,12 @@
+def triangleNumber(nums):
+    nums.sort()
+    count = 0
+    for i in range(len(nums) - 1, -1, -1):
+        left, right = 0, i - 1
+        while left < right:
+            if nums[left] + nums[right] > nums[i]:
+                count += right - left
+                right -= 1
+            else:
+                left += 1
+    return count
